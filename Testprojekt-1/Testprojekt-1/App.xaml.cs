71c4a8f27690f70cea216c5a84573d4393c5952c
Justin Rajakone <89641916+jrajakone@users.noreply.github.com>
2024-04-24 17:44:29 +0200

    Create Testproject

---
+++ Testprojekt-1/Testprojekt-1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Testprojekt_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
