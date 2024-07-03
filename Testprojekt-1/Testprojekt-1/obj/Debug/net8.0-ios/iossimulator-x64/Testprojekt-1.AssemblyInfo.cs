f9da606aca1764f9f1f292de82d7a69878077a1c
Justin Rajakone <89641916+jrajakone@users.noreply.github.com>
2024-07-03 12:52:42 +0200

    SideBar + Drag n Drop Zone

--- Testprojekt-1/Testprojekt-1/obj/Debug/net8.0-ios/iossimulator-x64/Testprojekt-1.AssemblyInfo.cs
+++ Testprojekt-1/Testprojekt-1/obj/Debug/net8.0-ios/iossimulator-x64/Testprojekt-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c843a5eed46b0b9c429002de9a8a7be12d27da65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c49c2139edaf458591feb7110281ac3f212c300")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
