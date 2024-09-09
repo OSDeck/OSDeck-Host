0c8d07aedc1a78a827788fb9018c902bc9fe2d65
Liam Wahlström <liam.wahlstroem@gmail.com>
2024-09-09 21:04:52 +0200

    Improved Comhandler

--- Testprojekt-1/Testprojekt-1/Helpers/ComHandler.cs
+++ Testprojekt-1/Testprojekt-1/Helpers/ComHandler.cs
@@ -11,43 +11,142 @@ namespace Testprojekt_1.Helpers
 {
     internal class ComHandler
     {
-        private SerialPort _serialPort;
+        private SerialPort _serialPort = null;
+        private Timer _timer;
+        private readonly int _interval = 30000;
+        private readonly int _baudrate = 9600;
+        private bool _isReading = false;
 
-        public ComHandler(string portName, int baudRate = 115200)
+        public ComHandler()
         {
-            _serialPort = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
-            _serialPort.Open();
+            StartScanning();
         }
 
-        public void SendData<T>(T data)
+        private void StartScanning()
+        {
+	        _timer = new Timer(async _ => FindDevice(), null, (long)0, _interval);
+        }
+
+        private void StartReading()
+        {
+			_timer = new Timer(async _ => ReceiveData(), null, (long)0, 100);
+        }
+
+        private void StopTimer()
+        {
+	        _timer.Dispose();
+        }
+
+		private async void FindDevice()
+		{
+			await Task.Run(() =>
+			{
+				//Test each port for a handshake response
+				foreach (string portName in SerialPort.GetPortNames())
+				{
+					try
+					{
+						using SerialPort port = new SerialPort(portName, _baudrate);
+						if (!port.IsOpen)
+						{
+							port.Open();
+						}
+						port.WriteLine("PC_HANDSHAKE");
+						Thread.Sleep(1000);
+
+						Console.WriteLine(portName);
+
+						if (port.BytesToRead > 0)
+						{
+							string response = port.ReadLine().Trim();
+							if (response == "ARDUINO_HANDSHAKE")
+							{
+								_serialPort = port;
+								StopTimer();
+								StartReading();
+								break;
+							}
+							else
+							{
+								port.Close();
+							}
+						}
+						else
+						{
+							port.Close();
+						}
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex);
+					}
+				}
+			});
+        }
+
+        public bool SendData<T>(T data)
         {
             if (_serialPort.IsOpen)
             {
-                string json = JsonConvert.SerializeObject(data);
-                _serialPort.WriteLine(json + "\n"); // Sende JSON plus neue Zeile, um das Ende der Übertragung zu kennzeichnen
+	            try
+	            {
+		            string json = JsonConvert.SerializeObject(data);
+		            _serialPort.WriteLine(json +
+		                                  "\n"); // Sende JSON plus neue Zeile, um das Ende der Übertragung zu kennzeichnen
+		            return true;
+	            }
+	            catch (Exception ex)
+	            {
+		            Console.WriteLine(ex);
+                    _serialPort.Close();
+                    _serialPort = null;
+                    StartScanning();
+                    return false;
+	            }
             }
             else
             {
-                Console.WriteLine("Serial port is not open.");
+                StartScanning();
+	            return false;
             }
         }
 
-        public string ReceiveData()
+        public async void ReceiveData()
         {
-            if (_serialPort.IsOpen && _serialPort.BytesToRead > 0)
-            {
-                string receivedJson = _serialPort.ReadLine();
-                return receivedJson;
-            }
-            return null;
-        }
+	        await Task.Run(() =>
+	        {
+		        if (_isReading) return;
 
-        public void Close()
-        {
-            if (_serialPort.IsOpen)
-            {
-                _serialPort.Close();
-            }
-        }
+		        _isReading = true;
+
+		        if (!_serialPort.IsOpen)
+		        {
+					StopTimer();
+				    StartScanning();
+			        _isReading = false;
+			        return;
+		        }
+
+				if (_serialPort.BytesToRead > 0)
+			    {
+		            try
+		            {
+						string receivedJson = _serialPort.ReadLine();
+						// DO something with data
+					}
+				    catch (Exception ex)
+			        {
+						_serialPort.Close();
+						_serialPort = null;
+						StopTimer();
+						StartScanning();
+						_isReading = false;
+						Console.WriteLine(ex);
+		            }
+	            }
+				
+	            _isReading = false;
+	        });
+		}
     }
 }
--- Testprojekt-1/Testprojekt-1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Testprojekt-1.AssemblyInfo.cs
+++ Testprojekt-1/Testprojekt-1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Testprojekt-1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+01e9f159b36627433ddb544fe4adb50d7fabbe2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+872c15067f38607fb195946bd4a9e88faf429721")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testprojekt-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
